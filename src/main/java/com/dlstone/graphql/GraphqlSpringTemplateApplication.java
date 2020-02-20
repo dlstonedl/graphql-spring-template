da8e5a066d46996e4262d46dc070c97dad6a3915
dlstonedl <ldeng@thoughtworks.com>
2020-02-20 13:44:44 +0800

    init project

---
+++ src/main/java/com/dlstone/graphql/GraphqlSpringTemplateApplication.java
@@ -0,0 +1,12 @@
+package com.dlstone.graphql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphqlSpringTemplateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GraphqlSpringTemplateApplication.class, args);
+    }
+}
