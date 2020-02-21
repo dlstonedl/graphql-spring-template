f533fbbb277dd7d9e255aa927ff38aac77edaa31
dlstonedl <ldeng@thoughtworks.com>
2020-02-21 17:33:35 +0800

    multi author

--- src/main/java/com/dlstone/graphql/common/GraphQLDataFetchers.java
+++ src/main/java/com/dlstone/graphql/common/GraphQLDataFetchers.java
@@ -1,30 +1,28 @@
 package com.dlstone.graphql.common;
 
 import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.Lists;
 import graphql.schema.DataFetcher;
-import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Component;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.stream.Collectors;
 
 @Component
 public class GraphQLDataFetchers {
-    private static List<Map<String, String>> books = Arrays.asList(
+    private static List<Map<String, Object>> books = Arrays.asList(
             ImmutableMap.of("id", "book-1",
                     "name", "Harry Potter and the Philosopher's Stone",
                     "pageCount", "223",
-                    "authorId", "author-1"),
+                    "authorIds", Lists.newArrayList("author-1", "author-2", "author-3")),
             ImmutableMap.of("id", "book-2",
                     "name", "Moby Dick",
                     "pageCount", "635",
-                    "authorId", "author-2"),
+                    "authorIds", Lists.newArrayList("author-1", "author-2", "author-3")),
             ImmutableMap.of("id", "book-3",
                     "name", "Interview with the vampire",
                     "pageCount", "371",
-                    "authorId", "author-3")
+                    "authorIds", Lists.newArrayList("author-1", "author-2", "author-3"))
     );
 
     private static List<Map<String, String>> authors = Arrays.asList(
@@ -44,7 +42,7 @@ public class GraphQLDataFetchers {
             String id = environment.getArgument("id");
             return books
                     .stream()
-                    .filter(book -> StringUtils.equals(book.get("id"), id))
+                    .filter(book -> Objects.equals(book.get("id"), id))
                     .findFirst()
                     .orElse(null);
         };
@@ -56,21 +54,21 @@ public class GraphQLDataFetchers {
 
     public DataFetcher getAuthorDataFetcher() {
         return environment -> {
-            Map<String,String> book = environment.getSource();
+            Map<String,Object> book = environment.getSource();
+            ArrayList<String> authorIds = (ArrayList<String>) book.get("authorIds");
             return authors
                     .stream()
-                    .filter(author -> StringUtils.equals(author.get("id"), book.get("authorId")))
-                    .findFirst()
-                    .orElse(null);
+                    .filter(author -> authorIds.contains(author.get("id")))
+                    .collect(Collectors.toList());
         };
     }
 
     public DataFetcher updateBookDataFetcher() {
         return environment -> {
-            Map<String, String> newBook = environment.getArgument("bookInput");
+            Map<String, Object> newBook = environment.getArgument("bookInput");
             books = books
                 .stream()
-                .filter(book -> !StringUtils.equals(book.get("id"), newBook.get("id")))
+                .filter(book -> !Objects.equals(book.get("id"), newBook.get("id")))
                 .collect(Collectors.toList());
             books.add(newBook);
             return newBook;
