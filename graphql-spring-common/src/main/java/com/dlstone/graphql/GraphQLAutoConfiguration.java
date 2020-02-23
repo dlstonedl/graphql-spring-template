0fb0fd16a4e810ba64dde496927f164373dc1f8f
dlstonedl <ldeng@thoughtworks.com>
2020-02-23 17:05:04 +0800

    move code

---
+++ graphql-spring-common/src/main/java/com/dlstone/graphql/GraphQLAutoConfiguration.java
@@ -0,0 +1,10 @@
+package com.dlstone.graphql;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.dlstone.graphql.common")
+public class GraphQLAutoConfiguration {
+
+}
