75e35cd4b924b683485d23ae79ca30bd32c9b75f
dlstonedl <ldeng@thoughtworks.com>
2020-02-23 12:08:53 +0800

    add graphql client

--- graphql-spring-webflux-template/src/main/java/com/dlstone/graphql/controller/GraphQLController.java
+++ graphql-spring-webflux-template/src/main/java/com/dlstone/graphql/controller/GraphQLController.java
@@ -1,10 +1,7 @@
 package com.dlstone.graphql.controller;
 
-import com.dlstone.graphql.common.DataLoaderRegistryFactory;
-import com.dlstone.graphql.common.GraphQLFactory;
+import com.dlstone.graphql.common.GraphQLClient;
 import com.dlstone.graphql.common.GraphQLRequest;
-import graphql.ExecutionInput;
-import graphql.GraphQL;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -13,35 +10,24 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
 
-import java.util.Collections;
 import java.util.Map;
 
 @Slf4j
 @RestController
 public class GraphQLController {
 
-    private GraphQL graphQL;
-
-    private DataLoaderRegistryFactory dataLoaderRegistryFactory;
+    private GraphQLClient graphQLClient;
 
     @Autowired
-    public GraphQLController(GraphQLFactory graphqlFactory, DataLoaderRegistryFactory dataLoaderRegistryFactory) {
-        this.graphQL = graphqlFactory.getGraphQL();
-        this.dataLoaderRegistryFactory = dataLoaderRegistryFactory;
+    public GraphQLController(GraphQLClient graphQLClient) {
+        this.graphQLClient = graphQLClient;
     }
 
     @PostMapping("/graphql")
     public Mono<ResponseEntity<Map<String, Object>>> graphql(@RequestBody Mono<GraphQLRequest> graphQLRequestMono) {
         return graphQLRequestMono
-            .map(graphQLRequest -> ExecutionInput.newExecutionInput()
-                    .query(graphQLRequest.getQuery())
-                    .operationName(graphQLRequest.getOperationName())
-                    .variables(graphQLRequest.getVariables() != null ? graphQLRequest.getVariables() : Collections.emptyMap())
-                    .dataLoaderRegistry(dataLoaderRegistryFactory.newDataLoaderRegistry())
-                    .build())
-            .flatMap(executionInput -> Mono.fromFuture(this.graphQL.executeAsync(executionInput)))
-            .map(executionResult -> ResponseEntity.ok().body(executionResult.toSpecification()))
+            .flatMap(graphQLRequest -> Mono.fromFuture(graphQLClient.invoke(graphQLRequest)))
+            .map(result -> ResponseEntity.ok().body(result))
             .switchIfEmpty(Mono.just(ResponseEntity.badRequest().build()));
     }
-
 }
