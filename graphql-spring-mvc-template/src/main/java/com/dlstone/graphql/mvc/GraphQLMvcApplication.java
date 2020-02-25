f7946e1f542f51b62fa2431bacde73c90bb0749b
dlstonedl <ldeng@thoughtworks.com>
2020-02-25 21:38:51 +0800

    rename

--- graphql-spring-mvc-template/src/main/java/com/dlstone/graphql/mvc/GraphQLMvcApplication.java
+++ graphql-spring-mvc-template/src/main/java/com/dlstone/graphql/mvc/GraphQLMvcApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class GraphqlSpringTemplateApplication {
+public class GraphQLMvcApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(GraphqlSpringTemplateApplication.class, args);
+        SpringApplication.run(GraphQLMvcApplication.class, args);
     }
 }
