16045bfefe355227e2d87147dc0939c99811cc9c
dlstonedl <ldeng@thoughtworks.com>
2020-02-26 21:16:44 +0800

    batch loader context

--- graphql-spring-mvc-template/src/main/java/com/dlstone/graphql/mvc/controller/DataLoadController.java
+++ graphql-spring-mvc-template/src/main/java/com/dlstone/graphql/mvc/controller/DataLoadController.java
@@ -3,12 +3,15 @@ package com.dlstone.graphql.mvc.controller;
 import com.dlstone.graphql.util.annotation.GraphQLController;
 import com.dlstone.graphql.util.annotation.LoaderMapping;
 import com.dlstone.graphql.mvc.repository.GraphQLRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.dataloader.BatchLoader;
+import org.dataloader.BatchLoaderWithContext;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.Map;
 import java.util.concurrent.CompletableFuture;
 
+@Slf4j
 @GraphQLController
 public class DataLoadController {
 
@@ -19,9 +22,19 @@ public class DataLoadController {
         this.graphQLRepository = graphQLRepository;
     }
 
-    @LoaderMapping(name = "author-loader")
+//    @LoaderMapping(name = "author-loader")
     public BatchLoader<String, Map<String, String>> authorBatchLoader() {
         return authorIds -> CompletableFuture.supplyAsync(() ->
             graphQLRepository.getAuthorsByIds(authorIds));
     }
+
+    @LoaderMapping(name = "author-loader")
+    public BatchLoaderWithContext<String, Map<String, String>> authorBatchLoaderWithContext() {
+        return (keys, environment) ->  CompletableFuture.supplyAsync(() -> {
+            Object context = environment.getContext();
+            log.info("context: " + context);
+            return graphQLRepository.getAuthorsByIds(keys);
+        });
+    }
+
 }
